0419aa66bc34a3103218eb4e847c76a47edb6739
Iskender Garajayev <yoloten.isa@gmail.com>
2024-01-01 03:10:01 -0800

    4

--- src/omponents/Home.jsx
+++ src/omponents/Home.jsx
@@ -1,11 +1,25 @@
 import React from 'react';
 import Header from './Header';
+import HomeImg from '../img/home-img.png';
 
 const Home = () => {
   return (
     <div className='home-container'>
       <Header />
-      <div className='home-inner'> home page</div>
+      <div className='home-inner'>
+        <div className='home-title'>
+          <p className='title'>
+            Kaliteli <span>Dijital Hizme</span>
+          </p>
+          <p className='slogan'>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus
+            quos nisi ex omnis adipisci sit. Hic beatae eligendi tenetur ipsa!
+          </p>
+        </div>
+        <div className='home-img'>
+          <img src={HomeImg} alt='' />
+        </div>
+      </div>
     </div>
   );
 };
